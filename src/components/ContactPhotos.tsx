6547c23915b4399d7c846d416df4ea7e99f0271c
Sven Günter <sven.guenter@homag.com>
2021-03-17 20:48:03 +0100

    implemented add photo feature

--- src/components/ContactPhotos.tsx
+++ src/components/ContactPhotos.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { ContactNotesComponentProps } from '../navigation/types';
-import { Alert, FlatList, ListRenderItem, ListRenderItemInfo, StyleSheet, Text, View} from 'react-native';
-import CameraRoll, { PhotoIdentifier } from '@react-native-community/cameraroll'
+import { Alert, Button, FlatList, ListRenderItem, ListRenderItemInfo, StyleSheet, Text, View} from 'react-native';
+import CameraRoll, { PhotoIdentifier, SaveToCameraRollOptions } from '@react-native-community/cameraroll'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import PhotoListRenderItem from './PhotoListRenderItem';
+import {Callback, CameraOptions, ImagePickerResponse, launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 
 
@@ -16,6 +17,9 @@ const ContactPhotos = ({route, navigation}: ContactNotesComponentProps) => {
             const params : CameraRoll.GetPhotosParams = {
                   first: 10,
                   assetType: 'Photos',
+                  groupTypes: 'Album',
+                  //groupTypes: 'All',
+                  groupName: route.params.contactId,
             };
             CameraRoll.getPhotos(params)
                 .then((r) => {
@@ -33,6 +37,40 @@ const ContactPhotos = ({route, navigation}: ContactNotesComponentProps) => {
         return <PhotoListRenderItem photo={photo.item}/>
     }
 
+    const onAddPhoto = function(){
+        const options : CameraOptions = {
+            mediaType: 'photo',
+        };
+        launchImageLibrary(options,onPhotoCallback);
+        
+        // Not possible due to Simulator
+        //launchCamera(options, onPhotoCallback);
+    }
+
+    const onPhotoCallback = function(response: ImagePickerResponse){
+        if(response.didCancel){
+            return;
+        }
+            
+        if(response.errorCode){
+            console.log(response.errorMessage);
+            return;
+        }
+        const options: SaveToCameraRollOptions = {
+            type: 'photo',
+            album: route.params.contactId,
+        }
+        if(response.uri !== undefined){
+            CameraRoll.save(response.uri,options).then(saveResult => {
+                setArePhotosLoaded(false);
+            }).catch(err => {
+                console.log(err);
+            })
+        }
+        
+        console.log(response);
+    }
+
     React.useEffect(loadPhotos, [arePhotosLoaded])
 
     return (
@@ -44,7 +82,16 @@ const ContactPhotos = ({route, navigation}: ContactNotesComponentProps) => {
             horizontal={false}
             numColumns={3}
             columnWrapperStyle={styles.columnWrapperStyle}
+            style={styles.list}
         />
+        <View style={styles.footer}>
+            <View style={styles.button}>
+                <Button
+                    onPress={onAddPhoto}
+                    title={"Add"}
+                />
+            </View>
+        </View>
     </View>
     );
 }
@@ -52,10 +99,23 @@ const ContactPhotos = ({route, navigation}: ContactNotesComponentProps) => {
 const styles = StyleSheet.create({
     container: {
         flex:1,
+        justifyContent: 'flex-end',
+    },
+    list:{
+        flex: 1,
     },
     columnWrapperStyle:{
         flex:1,
         justifyContent:'flex-start',
+    },
+    footer:{
+        flex:0.2,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    button:{
+        padding: 20,
+        marginRight: 20,
     }
 })
 
